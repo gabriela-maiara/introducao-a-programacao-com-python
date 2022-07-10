6d53a95fbd9d50302a7744b14d2285e059230f5a
gabriela-maiara <gabrielamaiara14@gmail.com>
2022-07-10 17:42:38 -0300

    -

---
+++ 3-9.py
@@ -0,0 +1,6 @@
+dias = int(input('Quantos dias? '))
+horas = int(input('Quantas horas? '))
+minutos = int(input('Quantos minutos? '))
+segundos = int(input('Quantos segundos? '))
+calculo = segundos + (minutos * 60) + (horas * 3600) + (dias * 86400)
+print(f'Total em segundos: {calculo}')
